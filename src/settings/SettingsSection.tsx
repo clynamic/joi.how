e451f6690a9704f973a654c75858312993933497
clragon <clragon@clynamic.net>
2024-04-20 04:22:40 +0200

    added settings tiles placeholders

---
+++ src/settings/SettingsSection.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { ContentSection } from '../home/components';
+import { SettingsTile } from './components';
+
+const StyledSettingsSection = styled(ContentSection)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+`;
+
+export const SettingsSection = () => {
+  return (
+    <StyledSettingsSection>
+      <SettingsTile label='Pace'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Duration'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Events'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Images'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Walltalker'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Climax'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Hypno'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Player'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Integration'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+      <SettingsTile label='Save/Load'>
+        lorem ipsum dolor sit amet consectetur adipisicing elit
+      </SettingsTile>
+    </StyledSettingsSection>
+  );
+};
