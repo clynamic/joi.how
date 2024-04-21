df5f80eb98037463ef24191824280afde81b0136
clragon <clragon@clynamic.net>
2024-04-22 00:34:24 +0200

    updated context provider to prevent unnecessary rebuilds

--- src/settings/SettingsProvider.tsx
+++ src/settings/SettingsProvider.tsx
@@ -1,14 +1,13 @@
-/* eslint-disable react-refresh/only-export-components */
-
 import { defaultSettings, Settings } from '../types';
 import { createLocalStorageProvider } from '../utils';
 
 const settingsStorageKey = 'settings';
 
-const { Provider: SettingsProvider, useLocalStorageHook: useSetting } =
-  createLocalStorageProvider<Settings>({
-    key: settingsStorageKey,
-    defaultData: defaultSettings,
-  });
-
-export { SettingsProvider, useSetting };
+export const {
+  Provider: SettingsProvider,
+  useProvider: useSettings,
+  useProviderSelector: useSetting,
+} = createLocalStorageProvider<Settings>({
+  key: settingsStorageKey,
+  defaultData: defaultSettings,
+});
