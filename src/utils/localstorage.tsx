f138c6c0eb0da91780efbfe5e59b43dda79008e6
clragon <clragon@clynamic.net>
2024-04-25 00:06:09 +0200

    added game image UI

--- src/utils/localstorage.tsx
+++ src/utils/localstorage.tsx
@@ -1,118 +1,65 @@
-import { useState, useEffect, PropsWithChildren, useCallback } from 'react';
-import {
-  createContext,
-  useContextSelector,
-  useContext,
-} from 'use-context-selector';
+import { PropsWithChildren, useEffect, useMemo } from 'react';
+import { createStateProvider } from './state';
 
 interface LocalStorageProviderOptions<T> {
   key: string;
   defaultData: T;
 }
 
-interface LocalStorageContextType<T> {
-  data: T;
-  setData: React.Dispatch<React.SetStateAction<T>>;
-}
+export function createLocalStorageProvider<T>({
+  key,
+  defaultData,
+}: LocalStorageProviderOptions<T>) {
+  const {
+    Provider: StateProvider,
+    useProvider,
+    useProviderSelector,
+  } = createStateProvider<T>({ defaultData: defaultData });
 
-function useLocalStorage<T>(
-  key: string,
-  initialValue: T
-): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      const parsed = item ? JSON.parse(item) : null;
-      if (typeof initialValue === 'object' && !Array.isArray(initialValue)) {
-        // this allows us to default on missing keys
-        return {
-          ...initialValue,
-          ...(parsed ?? {}),
-        };
-      } else {
-        return parsed ?? initialValue;
+  const LocalStorageProvider: React.FC<PropsWithChildren> = ({ children }) => {
+    const savedData = useMemo(() => {
+      try {
+        const item = window.localStorage.getItem(key);
+        const parsed = item ? JSON.parse(item) : null;
+        if (typeof defaultData === 'object' && !Array.isArray(defaultData)) {
+          // this allows us to default on missing keys
+          return {
+            ...defaultData,
+            ...(parsed ?? {}),
+          };
+        } else {
+          return parsed ?? defaultData;
+        }
+      } catch (error) {
+        console.error('Failed to read from localStorage:', error);
+        return defaultData;
       }
-    } catch (error) {
-      console.error('Failed to read from localStorage:', error);
-      return initialValue;
-    }
-  });
+    }, []);
 
-  useEffect(() => {
-    try {
-      window.localStorage.setItem(key, JSON.stringify(storedValue));
-    } catch (error) {
-      console.error('Failed to save to localStorage:', error);
-    }
-  }, [storedValue, key]);
+    const LocalStorageWriter: React.FC<PropsWithChildren> = ({ children }) => {
+      const [data] = useProvider();
 
-  return [storedValue, setStoredValue];
-}
+      useEffect(() => {
+        try {
+          window.localStorage.setItem(key, JSON.stringify(data));
+        } catch (error) {
+          console.error('Failed to save to localStorage:', error);
+        }
+      }, [data]);
 
-export function createLocalStorageProvider<T>(
-  options: LocalStorageProviderOptions<T>
-) {
-  const LocalStorageContext = createContext<
-    LocalStorageContextType<T> | undefined
-  >(undefined);
-
-  const Provider: React.FC<PropsWithChildren> = ({ children }) => {
-    const [data, setData] = useLocalStorage<T>(
-      options.key,
-      options.defaultData
-    );
-
-    const contextValue = { data, setData };
+      return children;
+    };
 
     return (
-      <LocalStorageContext.Provider value={contextValue}>
-        {children}
-      </LocalStorageContext.Provider>
+      <StateProvider defaultData={savedData}>
+        <LocalStorageWriter>{children}</LocalStorageWriter>
+      </StateProvider>
     );
   };
 
-  const useProvider = (): [T, React.Dispatch<React.SetStateAction<T>>] => {
-    const context = useContext(LocalStorageContext);
-    if (!context) {
-      throw new Error('useProvider must be used within its Provider');
-    }
-
-    return [context.data, context.setData];
+  return {
+    Provider: LocalStorageProvider,
+    useProvider,
+    useProviderSelector,
   };
-
-  /**
-   * Uses useContextSelector to select a specific key from the provider.
-   * This allows us to re-render only when the selected key changes.
-   * @param key
-   * @returns
-   */
-  function useProviderSelector<K extends keyof T>(
-    key: K
-  ): [T[K], (value: T[K]) => void] {
-    const selected = useContextSelector(LocalStorageContext, context => {
-      if (!context) {
-        throw new Error('useProvider must be used within its Provider');
-      }
-
-      return context.data[key];
-    });
-    const setData = useContextSelector(LocalStorageContext, context => {
-      if (!context) {
-        throw new Error('useProvider must be used within its Provider');
-      }
-
-      return context.setData;
-    });
-
-    const setSelected = useCallback(
-      (value: T[K]) => {
-        setData(prev => ({ ...prev, [key]: value }));
-      },
-      [setData, key]
-    );
-
-    return [selected, setSelected];
-  }
-
-  return { Provider, useProvider, useProviderSelector };
 }
