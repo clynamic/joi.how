51942f17c989fdf851435dc54d5e4826567466e0
clragon <clragon@clynamic.net>
2024-05-04 20:52:36 +0200

    fixed various home ui things

--- src/settings/components/ServiceSettings.tsx
+++ src/settings/components/ServiceSettings.tsx
@@ -3,11 +3,9 @@ import { E621Search } from '../../e621';
 
 export const ServiceSettings = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <SettingsTile label={'e621'}>
-        <SettingsDescription>Add images from e621</SettingsDescription>
-        <E621Search />
-      </SettingsTile>
-    </div>
+    <SettingsTile label={'e621'}>
+      <SettingsDescription>Add images from e621</SettingsDescription>
+      <E621Search />
+    </SettingsTile>
   );
 };
