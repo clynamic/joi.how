a7e3bb3881745c552f33e69c197c34658befc825
clragon <clragon@clynamic.net>
2024-05-04 20:52:36 +0200

    fixed various home ui things

--- src/home/components/Instructions.tsx
+++ src/home/components/Instructions.tsx
@@ -1,16 +1,20 @@
+import styled from 'styled-components';
 import { ContentSection } from '../../common';
+
+const StyledRulesTitle = styled.h2`
+  padding: 4px 16px;
+`;
 
 export const Instructions = () => {
   return (
     <ContentSection>
-      Some rules to keep to:
+      <StyledRulesTitle>Rules</StyledRulesTitle>
       <ul>
+        <li>Only focus on the porn on screen.</li>
         <li>
           Keep your strokes to the pace of the circle in the center of the
           screen.
         </li>
-        <li>Stroke using only the hand(s) shown in the top status bar.</li>
-        <li>Only focus on the porn on screen.</li>
         <li>Watch out for events announced on the right side.</li>
       </ul>
     </ContentSection>
