737f352e98a9107e47bf773331ad8d986774b3f8
clragon <clragon@clynamic.net>
2024-05-13 15:54:18 +0200

    fixed fullscreen error

--- src/utils/fullscreen.tsx
+++ src/utils/fullscreen.tsx
@@ -11,7 +11,7 @@ export const useFullscreen = () => {
         );
       });
     } else {
-      if (document.exitFullscreen) {
+      if (document.fullscreenElement) {
         document.exitFullscreen();
       }
     }
