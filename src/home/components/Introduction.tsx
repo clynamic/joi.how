bd79e0510310508ab2dd4f2cd368d72815a9fa70
clragon <clragon@clynamic.net>
2024-05-13 18:23:34 +0200

    fixed uppercase e in e621

--- src/home/components/Introduction.tsx
+++ src/home/components/Introduction.tsx
@@ -7,7 +7,7 @@ export const Introduction = () => {
         Select your settings, and this app will guide ya&apos; thru a jack-off
         session. You can adjust most of the aspects of the game, as well as
         select some porn to help with &quot;motivation&quot;. It&apos;s all
-        pulled from <a href='https://e621.net'>E621.net</a>.{' '}
+        pulled from <a href='https://e621.net'>e621.net</a>.{' '}
         <strong>You are responsible for what you choose to look at.</strong>
       </p>
     </ContentSection>
