1be70d3d4744d1ecf96b6290e75449ac46103533
clragon <clragon@clynamic.net>
2024-05-16 21:59:49 +0200

    removed logs from service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,8 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/service-worker.js')
-      .then(registration => {
-        console.log('SW registered: ', registration);
-      })
       .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
+        console.error('SW registration failed: ', registrationError);
       });
   });
 }
