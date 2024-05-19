b2438ab52b7e6f6dae602801f562f4ac3c2bda3d
Pup Gray <pupgray@outlook.com>
2024-05-19 13:46:14 -0400

    add a service for the walltaker actioncable api. Could use a library for this, but it's a really simple protocol

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '../home';
 import { GamePage } from '../game';
+import { WalltakerSocketServiceProvider } from '../utils/porn-socket/walltaker.tsx';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/play' element={<GamePage />} />
-      </Routes>
-    </BrowserRouter>
+    <WalltakerSocketServiceProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/play" element={<GamePage />} />
+        </Routes>
+      </BrowserRouter>
+    </WalltakerSocketServiceProvider>
   );
 };
