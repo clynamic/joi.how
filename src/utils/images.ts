fc59d161e35e1fca4000b8c224501ff39968a22a
clragon <clragon@clynamic.net>
2024-06-08 00:28:03 +0200

    added preloading videos

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,28 +1,47 @@
 import { useEffect } from 'react';
 import { ImageSize } from '../common';
-import { ImageItem } from '../types';
+import { ImageItem, ImageType } from '../types';
+
+interface PreloadItem {
+  src: string;
+  type: 'image' | 'video';
+}
+
+const preloadItems = (items: PreloadItem[]) => {
+  items.forEach(item => {
+    switch (item.type) {
+      case 'image': {
+        const img = new Image();
+        img.src = item.src;
+        break;
+      }
+      case 'video': {
+        const video = document.createElement('video');
+        video.src = item.src;
+        break;
+      }
+    }
+  });
+};
 
 export const useImagePreloader = (imageItems: ImageItem[], size: ImageSize) => {
   useEffect(() => {
-    const loadImages = (urls: string[]) => {
-      urls.forEach(url => {
-        const img = new Image();
-        img.src = url;
-      });
-    };
-
-    const urls: string[] = [];
+    const items: PreloadItem[] = [];
 
     imageItems.forEach(item => {
       if (size === ImageSize.thumbnail) {
-        urls.push(item.thumbnail);
+        items.push({ src: item.thumbnail, type: 'image' });
       } else if (size === ImageSize.preview) {
-        urls.push(item.thumbnail, item.preview);
+        items.push({ src: item.preview, type: 'image' });
       } else if (size === ImageSize.full) {
-        urls.push(item.thumbnail, item.preview, item.full);
+        if (item.type === ImageType.video) {
+          items.push({ src: item.full, type: 'video' });
+        } else {
+          items.push({ src: item.full, type: 'image' });
+        }
       }
     });
 
-    loadImages(urls);
+    preloadItems(items);
   }, [imageItems, size]);
 };
