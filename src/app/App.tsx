39da4f6897e50dc2c3cce0e198ea9298e46c85e7
clragon <clragon@clynamic.net>
2024-09-22 20:47:47 +0200

    moved walltalker provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '../home';
 import { GamePage } from '../game';
-import { WalltakerSocketServiceProvider } from '../utils/porn-socket/walltaker.tsx';
 
 export const App = () => {
   return (
-    <WalltakerSocketServiceProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/play' element={<GamePage />} />
-        </Routes>
-      </BrowserRouter>
-    </WalltakerSocketServiceProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/play' element={<GamePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,20 @@ import { SettingsProvider, ImageProvider } from './settings';
 import { E621Provider } from './e621';
 import { VibratorProvider } from './utils';
 import { LocalImageProvider } from './local/LocalProvider.tsx';
+import { WalltakerSocketServiceProvider } from './utils/porn-socket/walltaker.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SettingsProvider>
       <ImageProvider>
         <LocalImageProvider>
-          <VibratorProvider>
-            <E621Provider>
-              <App />
-            </E621Provider>
-          </VibratorProvider>
+          <WalltakerSocketServiceProvider>
+            <VibratorProvider>
+              <E621Provider>
+                <App />
+              </E621Provider>
+            </VibratorProvider>
+          </WalltakerSocketServiceProvider>
         </LocalImageProvider>
       </ImageProvider>
     </SettingsProvider>
