9ca6c32e31f84fcef79316d1f03baa74a4e7102b
Xephoidia <xephoidia@gmail.com>
2024-10-24 20:39:57 -0400

    Make actuator object update with UI

--- src/settings/components/ToyActuatorSettings.tsx
+++ src/settings/components/ToyActuatorSettings.tsx
@@ -1,9 +1,16 @@
 // import { useActuatorSettings } from '../../utils'
-import { ToyActuator } from '../../utils/toyactuator';
-import { PropsWithChildren } from 'react';
+import {
+  ToyActuator,
+  VibrationActuator,
+  ActuatorMode,
+  ActuatorModeLabels,
+} from '../../utils/toyactuator';
+import { PropsWithChildren, useState } from 'react';
 // import { Vibrator } from '../../utils/vibrator';
 import { SettingsTile } from '../../common';
 import { SettingsDescription } from '../../common/SettingsDescription';
+import { Dropdown } from '../../common/Dropdown';
+import { ActuatorType } from 'buttplug';
 
 export interface ToySettingsProps
   extends PropsWithChildren<React.HTMLAttributes<HTMLFieldSetElement>> {
@@ -13,14 +20,55 @@ export interface ToySettingsProps
 export const ToyActuatorSettings: React.FC<ToySettingsProps> = ({
   toyActuator,
 }) => {
-  const actuatorType = toyActuator.actuatorType;
-  const index = toyActuator.index;
+  return (
+    <SettingsTile
+      label={`${toyActuator.actuatorType} ${toyActuator.index + 1}`}
+    >
+      {(() => {
+        switch (toyActuator.actuatorType) {
+          case ActuatorType.Vibrate:
+            return <VibratorActuatorSettings toyActuator={toyActuator} />;
+          default:
+            return <UnknownActuatorSettings />;
+        }
+      })()}
+    </SettingsTile>
+  );
+};
+
+export const VibratorActuatorSettings: React.FC<ToySettingsProps> = ({
+  toyActuator,
+}) => {
+  const vibratorActuator = toyActuator as VibrationActuator;
+  const descriptor = `${vibratorActuator.actuatorType}_${vibratorActuator.index}`;
+  const [mode, setMode] = useState(vibratorActuator.mode);
 
   return (
-    <SettingsTile label={`${actuatorType} ${index + 1}`}>
+    <div key={`${descriptor}_settings`}>
       <SettingsDescription>
-        This component is not yet supported.
+        Select when this component will activate.
       </SettingsDescription>
-    </SettingsTile>
+      <Dropdown
+        id={`${descriptor}_mode`}
+        value={mode}
+        onChange={(value: string) => {
+          const newMode = value as ActuatorMode;
+          setMode(newMode);
+          vibratorActuator.setMode(newMode);
+        }}
+        options={Object.values(ActuatorMode).map(value => ({
+          value,
+          label: ActuatorModeLabels[value],
+        }))}
+      />
+    </div>
+  );
+};
+
+export const UnknownActuatorSettings = () => {
+  return (
+    <SettingsDescription>
+      This component is not currently supported.
+    </SettingsDescription>
   );
 };
