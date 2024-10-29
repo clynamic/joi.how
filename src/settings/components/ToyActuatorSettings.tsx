a8eea4d2cdccebbb1125760c96a723275eeba9a0
Xephoidia <xephoidia@gmail.com>
2024-10-28 23:40:25 -0400

    Add intensity range to vibrator control

--- src/settings/components/ToyActuatorSettings.tsx
+++ src/settings/components/ToyActuatorSettings.tsx
@@ -11,6 +11,8 @@ import { SettingsTile } from '../../common';
 import { SettingsDescription } from '../../common/SettingsDescription';
 import { Dropdown } from '../../common/Dropdown';
 import { ActuatorType } from 'buttplug';
+import { Space } from '../../common/Space';
+import { SettingsLabel } from '../../common/SettingsLabel';
 
 export interface ToySettingsProps
   extends PropsWithChildren<React.HTMLAttributes<HTMLFieldSetElement>> {
@@ -42,6 +44,8 @@ export const VibratorActuatorSettings: React.FC<ToySettingsProps> = ({
   const vibratorActuator = toyActuator as VibrationActuator;
   const descriptor = `${vibratorActuator.actuatorType}_${vibratorActuator.index}`;
   const [mode, setMode] = useState(vibratorActuator.mode);
+  const [min, setMin] = useState(vibratorActuator.minIntensity);
+  const [max, setMax] = useState(vibratorActuator.maxIntensity);
 
   return (
     <div key={`${descriptor}_settings`}>
@@ -59,6 +63,40 @@ export const VibratorActuatorSettings: React.FC<ToySettingsProps> = ({
         options={Object.values(ActuatorMode).map(value => ({
           value,
           label: ActuatorModeLabels[value],
+        }))}
+      />
+      <Space size='medium' />
+      <SettingsDescription>
+        Change the range of intensity for this component.
+      </SettingsDescription>
+      <SettingsLabel>From</SettingsLabel>
+      <Dropdown
+        id={`{${descriptor}_min}`}
+        value={`${min}`}
+        onChange={(value: string) => {
+          const newMin = +value;
+          if (newMin > vibratorActuator.maxIntensity) return;
+          setMin(newMin);
+          vibratorActuator.setMinIntensity(newMin);
+        }}
+        options={vibratorActuator.intensityRange.map(value => ({
+          value: `${value}`,
+          label: `${(value * 100).toFixed(0)}%`,
+        }))}
+      />
+      <SettingsLabel>To</SettingsLabel>
+      <Dropdown
+        id={`{${descriptor}_max}`}
+        value={`${max}`}
+        onChange={(value: string) => {
+          const newMax = +value;
+          if (newMax < vibratorActuator.minIntensity) return;
+          setMax(newMax);
+          vibratorActuator.setMaxIntensity(newMax);
+        }}
+        options={vibratorActuator.intensityRange.map(value => ({
+          value: `${value}`,
+          label: `${(value * 100).toFixed(0)}%`,
         }))}
       />
     </div>
