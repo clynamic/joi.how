4deeedf5530a8ac66d916f6757e7006f80aa12b5
Xephoidia <xephoidia@gmail.com>
2024-11-01 21:07:17 -0400

    Add LinearActuator class and supporting structures

--- src/utils/toyactuator.tsx
+++ src/utils/toyactuator.tsx
@@ -1,18 +1,30 @@
 import { type GenericDeviceMessageAttributes, ActuatorType } from 'buttplug';
 import { Stroke } from '../game/GameProvider';
 
-export enum ActuatorMode {
+export enum VibrateMode {
   alwaysOn = 'alwaysOn',
   alwaysOff = 'alwaysOff',
   activeUp = 'activeUp',
   activeDown = 'activeDown',
 }
 
-export const ActuatorModeLabels: Record<ActuatorMode, string> = {
-  [ActuatorMode.alwaysOn]: 'Always On',
-  [ActuatorMode.alwaysOff]: 'Always Off',
-  [ActuatorMode.activeUp]: 'Active on Upstroke',
-  [ActuatorMode.activeDown]: 'Active on Downstroke',
+export enum LinearMode {
+  alwaysOff = 'alwaysOff',
+  normal = 'normal',
+  inverted = 'inverted',
+}
+
+export const VibrateModeLabels: Record<VibrateMode, string> = {
+  [VibrateMode.alwaysOn]: 'Always On',
+  [VibrateMode.alwaysOff]: 'Always Off',
+  [VibrateMode.activeUp]: 'Active on Upstroke',
+  [VibrateMode.activeDown]: 'Active on Downstroke',
+};
+
+export const LinearModeLabels: Record<LinearMode, string> = {
+  [LinearMode.alwaysOff]: 'Always Off',
+  [LinearMode.normal]: 'Normal',
+  [LinearMode.inverted]: 'Inverted',
 };
 
 export class ToyActuator implements ToyActuatorSettings {
@@ -31,20 +43,20 @@ export class VibrationActuator
   extends ToyActuator
   implements VibrationActuatorSettings
 {
-  mode: ActuatorMode = ActuatorMode.activeUp;
+  mode: VibrateMode = VibrateMode.activeUp;
   minIntensity: number = 0;
   maxIntensity: number = 1.0;
   intensityRange: number[] = [];
 
   constructor(attributes: GenericDeviceMessageAttributes) {
     super(attributes);
-    const intensityStep = 100 / attributes.StepCount / 100.0;
+    const intensityStep = 100.0 / attributes.StepCount;
     for (let i = 0; i <= attributes.StepCount; ++i) {
       this.intensityRange[i] = intensityStep * i;
     }
   }
 
-  setMode(newMode: ActuatorMode) {
+  setMode(newMode: VibrateMode) {
     this.mode = newMode;
   }
 
@@ -59,20 +71,20 @@ export class VibrationActuator
   override getOutput(stroke: Stroke, intensity: number) {
     let output = this.minIntensity;
     switch (this.mode) {
-      case ActuatorMode.activeUp:
+      case VibrateMode.activeUp:
         if (stroke == Stroke.up) {
           output = this.mapToRange(intensity / 100);
         }
         break;
-      case ActuatorMode.activeDown:
+      case VibrateMode.activeDown:
         if (stroke == Stroke.down) {
           output = this.mapToRange(intensity / 100);
         }
         break;
-      case ActuatorMode.alwaysOn:
+      case VibrateMode.alwaysOn:
         output = this.mapToRange(intensity / 100);
         break;
-      case ActuatorMode.alwaysOff:
+      case VibrateMode.alwaysOff:
         output = 0;
         break;
     }
@@ -85,13 +97,71 @@ export class VibrationActuator
   }
 }
 
-export interface VibrationActuatorSettings {
-  mode: ActuatorMode;
-  minIntensity: number;
-  maxIntensity: number;
+export class LinearActuator
+  extends ToyActuator
+  implements LinearActuatorSettings
+{
+  mode: LinearMode = LinearMode.normal;
+  minPosition: number = 0;
+  maxPosition: number = 100;
+  positionRange: number[] = [];
+
+  constructor(attributes: GenericDeviceMessageAttributes) {
+    super(attributes);
+    const positionStep = 100.0 / attributes.StepCount;
+    for (let i = 0; i < attributes.StepCount; ++i) {
+      this.positionRange[i] = positionStep * i;
+    }
+  }
+
+  setMode(newMode: LinearMode) {
+    this.mode = newMode;
+  }
+
+  setMinPosition(newMin: number) {
+    this.minPosition = newMin;
+  }
+
+  setMaxPosition(newMax: number) {
+    this.maxPosition = newMax;
+  }
+
+  override getOutput(stroke: Stroke, intensity: number, pace: number) {
+    let targetPosition = this.minPosition;
+    let movementTime = 1000.0 / pace;
+    switch (this.mode) {
+      case LinearMode.normal:
+        if (stroke == Stroke.up) {
+          targetPosition = this.maxPosition;
+        }
+        break;
+      case LinearMode.inverted:
+        if (stroke == Stroke.down) {
+          targetPosition = this.maxPosition;
+        }
+        break;
+      case LinearMode.alwaysOff:
+        // multiply by intensity so we "use" the value
+        // unsure if/how to use intensity in this function
+        movementTime = 0 * intensity;
+    }
+    return [targetPosition, movementTime];
+  }
 }
 
 export interface ToyActuatorSettings {
   index: number;
   actuatorType: ActuatorType;
+}
+
+export interface VibrationActuatorSettings {
+  mode: VibrateMode;
+  minIntensity: number;
+  maxIntensity: number;
+}
+
+export interface LinearActuatorSettings {
+  mode: LinearMode;
+  minPosition: number;
+  maxPosition: number;
 }
