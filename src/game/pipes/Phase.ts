6dc483fa2d8d34c2b2e25696a25e40d016ce4e88
clragon <clragon@clynamic.net>
2025-08-25 02:28:03 +0200

    improved composer dsl

--- src/game/pipes/Phase.ts
+++ src/game/pipes/Phase.ts
@@ -27,16 +27,15 @@ const PLUGIN_NAMESPACE = 'core.phase';
 export const setPhase: PipeTransformer<[GamePhase]> = phase =>
   Composer.set<GamePhase>(['state', PLUGIN_NAMESPACE, 'current'], phase);
 
-export const phasePipe: Pipe = Composer.chain(frame =>
-  frame
-    .zoom<GameState>('state', state =>
-      state.unless(state.get(PLUGIN_NAMESPACE) != null, state =>
-        state.set<PhaseState>(PLUGIN_NAMESPACE, {
-          current: GamePhase.warmup,
-        })
-      )
+export const phasePipe: Pipe = Composer.pipe(
+  Composer.zoom<GameState>('state', state =>
+    state.unless(state.get(PLUGIN_NAMESPACE) != null, state =>
+      state.set<PhaseState>(PLUGIN_NAMESPACE, {
+        current: GamePhase.warmup,
+      })
     )
-    .zoom<GameContext>('context', context =>
-      context.set(PLUGIN_NAMESPACE, { setPhase })
-    )
+  ),
+  Composer.zoom<GameContext>('context', context =>
+    context.set(PLUGIN_NAMESPACE, { setPhase })
+  )
 );
