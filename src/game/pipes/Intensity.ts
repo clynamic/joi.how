5584b7136479a2de03ce57613230bf69ed2f1adf
clragon <clragon@clynamic.net>
2025-08-25 13:04:43 +0200

    made intensity phase aware

--- src/game/pipes/Intensity.ts
+++ src/game/pipes/Intensity.ts
@@ -1,4 +1,6 @@
-import { Composer, GameContext, Pipe } from '../../engine';
+import { Composer, Pipe } from '../../engine';
+import { Settings } from '../../settings';
+import { GamePhase } from './Phase';
 
 const PLUGIN_NAMESPACE = 'core.intensity';
 
@@ -6,18 +8,23 @@ export type IntensityState = {
   intensity: number;
 };
 
-export const intensityPipe: Pipe = Composer.bind<number>(
-  ['context', 'deltaTime'],
-  delta =>
-    Composer.bind<GameContext>(['context'], ({ settings }) =>
-      Composer.over<IntensityState>(
-        ['state', PLUGIN_NAMESPACE],
-        ({ intensity = 0 }) => ({
-          intensity: Math.min(
-            1,
-            intensity + delta / (settings.gameDuration * 1000)
-          ),
-        })
+export const intensityPipe: Pipe = Composer.bind<GamePhase>(
+  ['state', 'core.phase', 'current'],
+  currentPhase =>
+    Composer.when(
+      currentPhase === GamePhase.active,
+      Composer.bind<number>(['context', 'deltaTime'], delta =>
+        Composer.bind<Settings>(['context', 'settings'], settings =>
+          Composer.over<IntensityState>(
+            ['state', PLUGIN_NAMESPACE],
+            ({ intensity = 0 }) => ({
+              intensity: Math.min(
+                1,
+                intensity + delta / (settings.gameDuration * 1000)
+              ),
+            })
+          )
+        )
       )
     )
 );
